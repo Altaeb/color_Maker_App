5dbcf344a0ae295b749f0afa3409c47a35a34df8
aftiyah <abd2010ly@gmail.com>
2020-04-03 16:48:55 +0200

    Color Maker with Sliders

--- ColorMaker/ViewController.swift
+++ ColorMaker/ViewController.swift
@@ -15,9 +15,9 @@ class ViewController: UIViewController {
     // MARK: Outlets
     
     @IBOutlet weak var colorView: UIView!
-    @IBOutlet weak var redControl: UISwitch!
-    @IBOutlet weak var greenControl: UISwitch!
-    @IBOutlet weak var blueControl: UISwitch!
+    @IBOutlet weak var redControl: UISlider!
+    @IBOutlet weak var greenControl: UISlider!
+    @IBOutlet weak var blueControl: UISlider!
 
     // MARK: Life Cycle
     
@@ -37,9 +37,9 @@ class ViewController: UIViewController {
             return
         }
         
-        let r: CGFloat = self.redControl.isOn ? 1 : 0
-        let g: CGFloat = self.greenControl.isOn ? 1 : 0
-        let b: CGFloat = self.blueControl.isOn ? 1 : 0
+        let r: CGFloat = CGFloat(self.redControl!.value)
+        let g: CGFloat = CGFloat(self.greenControl!.value)
+        let b: CGFloat = CGFloat(self.blueControl!.value)
                 
         colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
     }
